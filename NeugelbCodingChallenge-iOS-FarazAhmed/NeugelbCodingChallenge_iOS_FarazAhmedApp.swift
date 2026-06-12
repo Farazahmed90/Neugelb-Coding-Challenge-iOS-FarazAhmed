e8912d341ab16d780ea9bd77585ede67669672ab
Faraz Ahmed <khatrifaraz40@gmail.com>
2026-06-12 17:24:45 +0200

    Initial Commit

---
+++ NeugelbCodingChallenge-iOS-FarazAhmed/NeugelbCodingChallenge_iOS_FarazAhmedApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeugelbCodingChallenge_iOS_FarazAhmedApp.swift
+//  NeugelbCodingChallenge-iOS-FarazAhmed
+//
+//  Created by FarazAhmed on 12.06.26.
+//
+
+import SwiftUI
+
+@main
+struct NeugelbCodingChallenge_iOS_FarazAhmedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
